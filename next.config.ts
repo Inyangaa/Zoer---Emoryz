2d5d6180a06d34b911df39a71a9369be165f529c
Inyangaa <xobofactors@aol.com>
2025-11-16 16:05:56 -0500

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['localhost', 'oaidalleapiprodscus.blob.core.windows.net'],
+  },
+}
+
+export default nextConfig
